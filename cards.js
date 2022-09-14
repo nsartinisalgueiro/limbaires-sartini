5d4129a2b618f95c525667c250ec35c453ad869b
Nahuel Sartini <nahuel.sartini@gmail.com>
2022-09-14 18:04:24 -0300

    se modifico el fetch

--- cards.js
+++ cards.js
@@ -1,42 +1,22 @@
-const divArticulos = document.querySelector("#cardArticulos");
+const divClientes = document.querySelector("#clientes");
 
-
-
-function crearCard(imagen, producto, cantidad, precio) {
-    return (
-        `
-        <div class="card m-3 col-3" style="width: 18rem;" data-aos="zoom-in">
-                <img src="../img/${imagen}" class="card-img-top img-fluid mt-2" alt="cargando-imagen">
+function crearCard(nombre, apellido, edad, comentario) {
+    return `
+        <div class="card m-3 mt-5 d-flex flex-row col-3" style="width: 18rem;" data-aos="zoom-in">
             <div class="card-body mb-4">
-                <h5 class="card-title fw-bold d-flex justify-content-center">${producto}</h5>
-                <p class="card-text fst-italic fw-semibold d-flex justify-content-center">${cantidad}</p>
-                <p class="card-text fst-italic fw-semibold d-flex justify-content-center">${precio}</p>
-            </div>
-            <div class="d-flex align-items-center justify-content-center">
-                <button class="btnMenos btn btn-secondary" data-producto="${producto}">-</button>
-                <div class="qty mx-2 border border-4 px-2 py-1" data-producto="${producto}">0</div>
-                <button class="btnMas btn btn-secondary" data-producto="${producto}">+</button>
+                <h5 class="card-title fw-bold d-flex justify-content-center">Nombre: ${nombre} ${apellido}</h5>
+                <p class="card-text fst-italic fw-semibold d-flex justify-content-center">Edad: ${edad}</p>
+                <p class="card-text fst-italic fw-semibold d-flex justify-content-center">"${comentario}"</p>
             </div>
         </div>
-        `
-    )
+        `;
 }
 
 fetch("./articulos.json")
     .then((response) => response.json())
     .then((data) => {
-        data.items.articulos.forEach(el => {
-            let detalle = crearCard(el.imagen, el.producto, el.cantidad, el.precio);
-            divArticulos.innerHTML += detalle
-        })
-        const btnMenos = document.querySelectorAll(".btnMenos");
-        const btnMas = document.querySelectorAll(".btnMas");
-        
-        btnMenos.forEach(element => element.addEventListener("click", function () {
-            disminuirCant(this)
-        }));
-        btnMas.forEach(element => element.addEventListener("click", function () {
-            aumentarCant(this)
-        }));
-        console.log(btnMenos)
-    })
+        data.items.articulos.forEach((el) => {
+            let detalle = crearCard(el.nombre, el.apellido, el.edad, el.comentario);
+            divClientes.innerHTML += detalle;
+        });
+    });
--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const btnPreview = document.querySelector("#btnPreview");
 const btnOnlyCarrito = document.querySelectorAll(".onlyIfCarrito");
 const modalBuy = document.querySelector("#modalBuy");
 const wsappNum = "541136398820";
-
+const btnMenos = document.querySelectorAll(".btnMenos");
+const btnMas = document.querySelectorAll(".btnMas");
 
 // let nombreCliente = JSON.parse(localStorage.getItem("nombreCte")) == null ? preguntarNombre() : JSON.parse(localStorage.getItem("nombreCte"));
 
@@ -13,29 +14,24 @@ const wsappNum = "541136398820";
 //     return nombre;
 // };
 
-
-
-
-
-function refresh(){
+function refresh() {
     qtyBoxArr.forEach(function (el) {
-        let  producto = el.dataset.producto
-        if (localStorage.getItem(producto)){
-            el.innerHTML = localStorage.getItem(producto)
+        let producto = el.dataset.producto;
+        if (localStorage.getItem(producto)) {
+            el.innerHTML = localStorage.getItem(producto);
         }
-    })
-};
-
-refresh()
-
-function vaciarLocalStorage(){
-    Object.keys(localStorage).forEach(function(el){
-        if(el !== "nombreCte" ){
-            localStorage.removeItem(el)
-        }
-    })
+    });
 }
 
+refresh();
+
+function vaciarLocalStorage() {
+    Object.keys(localStorage).forEach(function (el) {
+        if (el !== "nombreCte") {
+            localStorage.removeItem(el);
+        }
+    });
+}
 
 let qtyBox;
 
@@ -43,71 +39,74 @@ function aumentarCant(ev) {
     let producto = ev.dataset.producto;
     qtyBoxArr.forEach(function (el) {
         if (el.dataset.producto == producto) {
-            qtyBox = el
+            qtyBox = el;
         }
-    })
+    });
     let cantidad = parseInt(qtyBox.innerHTML);
     cantidad++;
     qtyBox.innerHTML = cantidad;
     console.log("Se añadio un producto " + producto);
-    localStorage.setItem(producto,JSON.stringify(cantidad))
-};
-
+    localStorage.setItem(producto, JSON.stringify(cantidad));
+}
 
 function disminuirCant(ev) {
     let producto = ev.dataset.producto;
     qtyBoxArr.forEach(function (el) {
         if (el.dataset.producto == producto) {
-            qtyBox = el
+            qtyBox = el;
         }
-    })
+    });
     let cantidad = parseInt(qtyBox.innerHTML);
-    cantidad == 0 ? cantidad = 0 : cantidad--;
+    cantidad == 0 ? (cantidad = 0) : cantidad--;
     qtyBox.innerHTML = cantidad;
     console.log("Se quitó un producto " + producto);
-    localStorage.setItem(producto, JSON.stringify(cantidad))
-};
+    localStorage.setItem(producto, JSON.stringify(cantidad));
+}
 
 function showCarrito() {
     modalBuy.classList.remove("bg-success");
     let qtyArray = Array.from(qtyBoxArr);
-    if (qtyArray.some(function (el) {
+    if (
+        qtyArray.some(function (el) {
             return parseInt(el.innerHTML) > 0;
-        })) {
-        btnOnlyCarrito.forEach(el => el.style.display = "inline");
+        })
+    ) {
+        btnOnlyCarrito.forEach((el) => (el.style.display = "inline"));
         let msg = "Vas a comprar:<br>";
         qtyBoxArr.forEach(function (el) {
             if (el.innerHTML > 0) {
-                msg += el.innerHTML + " De " + el.dataset.producto + "<br>"
+                msg += el.innerHTML + " De " + el.dataset.producto + "<br>";
             }
         });
         modalBuy.innerHTML = msg;
     } else {
-        btnOnlyCarrito.forEach(el => el.style.display = "none");
-        modalBuy.innerHTML = "No hay productos en el carrito, añade algo para comprar!";
-    };
-};
+        btnOnlyCarrito.forEach((el) => (el.style.display = "none"));
+        modalBuy.innerHTML =
+            "No hay productos en el carrito, añade algo para comprar!";
+    }
+}
 
 function emptyCart() {
-    btnOnlyCarrito.forEach(el => el.style.display = "none");
-    modalBuy.innerHTML = "No hay productos en el carrito, añade algo para comprar!";
-    qtyBoxArr.forEach(el => el.innerHTML = 0);
+    btnOnlyCarrito.forEach((el) => (el.style.display = "none"));
+    modalBuy.innerHTML =
+        "No hay productos en el carrito, añade algo para comprar!";
+    qtyBoxArr.forEach((el) => (el.innerHTML = 0));
     vaciarLocalStorage();
-};
+}
 
 function buyCart() {
     let texto = "Hola,%20quiero%20relizar%20un%20pedido%20de:%0A";
     qtyBoxArr.forEach(function (el) {
         if (el.innerHTML > 0) {
-            texto += `${el.innerHTML}%20X%20${el.dataset.producto}%0A`
+            texto += `${el.innerHTML}%20X%20${el.dataset.producto}%0A`;
         }
     });
     let wsappLink = `https://wa.me/${wsappNum}?text=${texto}`;
     window.open(wsappLink, "_blank");
-    btnOnlyCarrito.forEach(el => el.style.display = "none");
-    modalBuy.classList.add("bg-success")
+    btnOnlyCarrito.forEach((el) => (el.style.display = "none"));
+    modalBuy.classList.add("bg-success");
     modalBuy.innerHTML = "Gracias por realizar la compra!";
-    qtyBoxArr.forEach(el => el.innerHTML = 0);
+    qtyBoxArr.forEach((el) => (el.innerHTML = 0));
     vaciarLocalStorage();
 }
 
@@ -116,13 +115,23 @@ function buyCart() {
 btnPreview.addEventListener("click", showCarrito);
 btnOnlyCarrito.forEach(function (el) {
     if (el.dataset.btntype == "emptyCart") {
-        el.addEventListener("click", emptyCart)
+        el.addEventListener("click", emptyCart);
     }
 });
 btnOnlyCarrito.forEach(function (el) {
     if (el.dataset.btntype == "buyCart") {
-        el.addEventListener("click", buyCart)
+        el.addEventListener("click", buyCart);
     }
 });
 
-btnMas.forEach(el => console.log(el.dataset.producto))
+btnMenos.forEach((element) =>
+    element.addEventListener("click", function () {
+        disminuirCant(this);
+    })
+);
+btnMas.forEach((element) =>
+    element.addEventListener("click", function () {
+        aumentarCant(this);
+    })
+);
+
